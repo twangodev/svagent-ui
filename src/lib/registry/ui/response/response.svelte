11ddcfbe96787be56f5ca5e42ec9db3e4880360e
James Ding <jamesding365@gmail.com>
2026-04-06 11:15:51 -0700

    feat: add Response component with Streamdown integration

---
+++ src/lib/registry/ui/response/response.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { cn } from "$lib/utils.js";
+	import { Streamdown, type StreamdownProps } from "svelte-streamdown";
+
+	let { class: className, ...restProps }: StreamdownProps & { class?: string } = $props();
+</script>
+
+<Streamdown
+	class={cn("size-full [&>*:first-child]:mt-0 [&>*:last-child]:mb-0", className)}
+	{...restProps}
+/>
